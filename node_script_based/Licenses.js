62a924da4eb0b45a3cd5677416be4b2f81300b40
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2019-07-24 15:40:46 +0200

    move json file import for component from import to prop

--- node_script_based/Licenses.js
+++ node_script_based/Licenses.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import licensesJson from '#data/licenses.json'
 
 const LicenseInfo = ({
   name,
@@ -13,7 +12,7 @@ const LicenseInfo = ({
     <div className="license__first-row">
       <span className="license__name">{name}</span>
       <span className="license__version">{`@${version}`}</span>
-      <span className="license__author">{`(${author})`}</span>
+      {author && <span className="license__author">{`(${author})`}</span>}
     </div>
     <div className="license__license">{`Lizenz: ${license}`}</div>
     <div className="license__text">{licenseText}</div>
@@ -24,15 +23,18 @@ LicenseInfo.propTypes = {
   name: PropTypes.string,
   version: PropTypes.string,
   author: PropTypes.string,
-  license: PropTypes.string,
+  license: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   licenseText: PropTypes.string,
 }
 
-const Licenses = () => (
+const Licenses = ({ licensesJson }) => (
   <div className="licenses">
     {licensesJson.map(info => (<LicenseInfo key={info.name + info.version} {...info} />))}
   </div>
 )
 
+Licenses.propTypes = {
+  licensesJson: PropTypes.array,
+}
 
 export default Licenses
